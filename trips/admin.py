3a52e8ae79cd81dcd986b5abdb68299b01d3556f
mandelak <kazi.keith@gmail.com>
2021-07-12 15:01:28 +0300

    Set Up Project
    - Set up Django Project
    - Update database configurations
    - Create custom user model
    - Update UserAdmin
    - Run first migrations on trips

---
+++ trips/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    pass
