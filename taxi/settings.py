5157e85e65e6932cb05cb30d17591d1eea6d7acb
Keith <kazi.keith@gmail.com>
2021-07-13 16:41:20 +0300

    Set up django heroku (#2)
    * Set up django heroku
    
    * Add Procfile
    
    * Set up poetry
    
    * Specify specific python version to be used

--- taxi/settings.py
+++ taxi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,6 @@ CHANNEL_LAYERS = {
         }
     }
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
